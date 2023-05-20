785c3a03da10d41aeeab2ea0940e26e637a04490
Tobias <juantobiasdl7@gmail.com>
2023-05-20 11:12:42 -0600

    first commit

---
+++ src/Components/Card-list/Card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+    render() {
+        return <div> hello im0m the CardList Component</div>
+    }
+}
+
+export default CardList;
---
+++ src/Components/Search-bar/Search-bar.component.jsx
@@ -0,0 +1,5 @@
+import { Component } from 'react';
+
+class SearchBar extends Component {
+
+}
